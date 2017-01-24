8f540ec016dfc150b09bab35d8607635bed62c16
dinesh.gupta <dinesh.gupta@tavant.com>
2017-01-24 13:37:10 +0530

    Added one sop method.

--- CoreJ/src/StrEquals.java
+++ CoreJ/src/StrEquals.java
@@ -3,5 +3,9 @@ public class StrEquals {
 	public static void main(String[] args) {
 		System.out.println("hello world...");
 		System.out.println("testing...");
+		new StrEquals().printMsg("New in git");
+	}
+	void printMsg(String str){
+		System.out.println(str);
 	}
 }
