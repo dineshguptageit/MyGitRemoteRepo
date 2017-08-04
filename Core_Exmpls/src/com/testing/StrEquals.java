5629990a54f793a48efe20c8b0afc034bf4239ef
dinesh.gupta <dinesh.gupta@E5470-01458.us.corp.tavant.com>
2017-08-04 18:48:44 -0400

    test

---
+++ Core_Exmpls/src/com/testing/StrEquals.java
@@ -0,0 +1,9 @@
+package com.testing;
+
+public class StrEquals {
+	public static void main(String[] args) {
+		System.out.println("hello...");
+		System.out.println("Commit to local Repo...");
+		System.out.println("testing new branch.123..");
+	}
+}
